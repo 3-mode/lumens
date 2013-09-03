5b1c55864fe4fd8dd1376820fa65a4dffc2abc9e
shaofeng.wang@outlook.com <>
2013-09-04 01:06:12 +0800

    add 人员类别, 身份类别 data, modify the data types and field name

---
+++ dbdesign/sql/身份类别_data.sql
@@ -0,0 +1,3 @@
+Insert into DICT_身份类别 (身份类别名称,身份类别代码) values ('正式职工','99');
+Insert into DICT_身份类别 (身份类别名称,身份类别代码) values ('外聘职工','18');
+Insert into DICT_身份类别 (身份类别名称,身份类别代码) values ('离退休人员等','16');
