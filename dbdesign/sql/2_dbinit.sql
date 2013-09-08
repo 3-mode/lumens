4d78b814e7a7202020b24bad6a9117f23a046d0d
shaofeng.wang@outlook.com <>
2013-09-08 20:34:15 +0800

    add entity table, refine and modify wrong characters in the file

--- dbdesign/sql/2_dbinit.sql
+++ dbdesign/sql/2_dbinit.sql
@@ -1,4 +1,4 @@
-/* Entity table */
+/* Entity tables */
 DROP TABLE 个人概况 CASCADE CONSTRAINTS;
 DROP TABLE 人员性质 CASCADE CONSTRAINTS;
 DROP TABLE 专业技术职称 CASCADE CONSTRAINTS;
@@ -6,7 +6,21 @@ DROP TABLE 来校信息 CASCADE CONSTRAINTS;
 DROP TABLE 任职单位 CASCADE CONSTRAINTS;
 DROP TABLE 政治面貌 CASCADE CONSTRAINTS;
 DROP TABLE 学历学位 CASCADE CONSTRAINTS;
-/* Dict table */
+DROP TABLE 出国进修 CASCADE CONSTRAINTS;
+DROP TABLE 国内进修培训 CASCADE CONSTRAINTS;
+DROP TABLE 家庭成员 CASCADE CONSTRAINTS;
+DROP TABLE 工人技术等级 CASCADE CONSTRAINTS;
+DROP TABLE 惩处 CASCADE CONSTRAINTS;
+DROP TABLE 指导博士后 CASCADE CONSTRAINTS;
+DROP TABLE 指导国内访问学者 CASCADE CONSTRAINTS;
+DROP TABLE 来校前工作简历 CASCADE CONSTRAINTS;
+DROP TABLE 校内异动 CASCADE CONSTRAINTS;
+DROP TABLE 离校 CASCADE CONSTRAINTS;
+DROP TABLE 考核 CASCADE CONSTRAINTS;
+DROP TABLE 联系方式 CASCADE CONSTRAINTS;
+DROP TABLE 聘用合同 CASCADE CONSTRAINTS;
+DROP TABLE 荣誉性奖励 CASCADE CONSTRAINTS;
+/* Dict tables */
 DROP TABLE DICT_职称 CASCADE CONSTRAINTS;
 DROP TABLE DICT_党政职务 CASCADE CONSTRAINTS;
 DROP TABLE DICT_人员类别 CASCADE CONSTRAINTS;
@@ -155,35 +169,340 @@ CREATE TABLE 专业技术职称
     CONSTRAINT 专业技术职称 UNIQUE (职工号, 职称)
   );
 /*==============================================================*/
+/* Table: 任职单位                                                  */
+/*==============================================================*/
+CREATE TABLE 任职单位
+  (
+    全局编号     VARCHAR2(64),
+    职工号      VARCHAR2(10) NOT NULL,
+    最后修改时间   VARCHAR2(25),
+    人事关系所在单位 NUMBER(1, 0),
+    调动时间     DATE,
+    单位       VARCHAR2(255) NOT NULL,
+    岗位       VARCHAR2(100),
+    科室       VARCHAR2(100),
+    学科       VARCHAR2(255),
+    专业       VARCHAR2(255),
+    科室名称     VARCHAR2(120),
+    备注       VARCHAR2(500),
+    从事学科     SMALLINT,
+    从事专业     VARCHAR2(6),
+    主要岗位     SMALLINT,
+    兼职岗位     SMALLINT,
+    CONSTRAINT 任职单位 PRIMARY KEY (职工号, 单位)
+  );
+/*==============================================================*/
+/* Table: 党政职务
+/*==============================================================*/
+CREATE TABLE 党政职务
+  (
+    全局编号     VARCHAR2(64),
+    职工号      VARCHAR2(10),
+    最后修改时间   VARCHAR2(25),
+    职务类别     SMALLINT,
+    职务名称     SMALLINT,
+    任职日期     DATE,
+    任职部门     VARCHAR2(120),
+    任职方式     NUMBER(1,0),
+    任职原因     VARCHAR2(160),
+    任职文号     VARCHAR2(48),
+    职务级别     SMALLINT,
+    职位分类     SMALLINT,
+    任职批准单位   VARCHAR2(120),
+    职务变动类别   SMALLINT,
+    当前任职状况   NUMBER(1,0),
+    职务排序     VARCHAR2(4),
+    主管或从事的工作 VARCHAR2(80),
+    免职日期     DATE,
+    免职方式     NUMBER(1,0),
+    免职原因     NUMBER(1,0),
+    免职文号     VARCHAR2(48),
+    免职批准单位   VARCHAR2(120),
+    备注       VARCHAR2(500)
+  );
+/*==============================================================*/
+/* Table: 出国（境）进修
+/*==============================================================*/
+CREATE TABLE 出国进修
+  (
+    全局编号      VARCHAR2(64),
+    职工号       VARCHAR2(10),
+    最后修改时间    VARCHAR2(25),
+    出国日期      DATE,
+    出国目的      VARCHAR2(360),
+    出国国别      SMALLINT,
+    所去单位英文名称  VARCHAR2(360),
+    所去单位中文名称  VARCHAR2(240),
+    指导教师      VARCHAR2(120),
+    指导教师EMAIL VARCHAR2(240),
+    派出单位      VARCHAR2(120),
+    经费项目渠道    VARCHAR2(120),
+    出国经费来源    NUMBER(1,0),
+    审批单位      VARCHAR2(120),
+    审批日期      DATE,
+    审批文号      VARCHAR2(48),
+    批准期限      VARCHAR2(3),
+    批延期限      VARCHAR2(3),
+    学习工作内容    VARCHAR2(160),
+    学习工作业绩    VARCHAR2(2400),
+    学习方式      SMALLINT,
+    应归日期      DATE,
+    状态        SMALLINT,
+    相应日期      DATE,
+    经济关系      NUMBER(1,0),
+    停薪年月      DATE,
+    恢复薪金年月    DATE,
+    担保人       DATE,
+    担保人所在单位   SMALLINT,
+    回国入境日期    DATE,
+    备注        VARCHAR2(500)
+  );
+/*==============================================================*/
+/* Table: 国内进修培训
+/*==============================================================*/
+CREATE TABLE 国内进修培训
+  (
+    全局编号   VARCHAR2(64),
+    职工号    VARCHAR2(10),
+    最后修改时间 VARCHAR2(25),
+    进修性质   SMALLINT,
+    学习方式   SMALLINT,
+    学习起始年月 DATE,
+    学习终止年月 DATE,
+    总学时    SMALLINT,
+    学习内容   VARCHAR2(160),
+    进修班名称  VARCHAR2(80),
+    主办单位   VARCHAR2(120),
+    主办单位性质 SMALLINT,
+    在学单位   VARCHAR2(120),
+    在学单位类别 NUMBER(1,0),
+    进修结果   NUMBER(1,0),
+    备注     VARCHAR2(500)
+  );
+/*==============================================================*/
+/* Table: 家庭成员
+/*==============================================================*/
+CREATE TABLE 家庭成员
+  (
+    全局编号   VARCHAR2(64),
+    职工号    VARCHAR2(10),
+    最后修改时间 VARCHAR2(25),
+    主要成员姓名 VARCHAR2(60),
+    称谓     VARCHAR2(100),
+    出生时间   DATE,
+    工作单位   VARCHAR2(120),
+    备注     VARCHAR2(500)
+  );
+/*==============================================================*/
+/* Table: 工人技术等级
+/*==============================================================*/
+CREATE TABLE 工人技术等级
+  (
+    全局编号     VARCHAR2(64),
+    职工号      VARCHAR2(10),
+    最后修改时间   VARCHAR2(25),
+    现技术等级    NUMBER(1,0),
+    工人技术等级资格 NUMBER(1,0),
+    资格取得日期   DATE,
+    取得资格途径   VARCHAR2(100),
+    资格评审单位   CHAR(10),
+    聘用工人技术等级 NUMBER(1,0),
+    起聘日期     DATE,
+    聘用单位     VARCHAR2(120),
+    聘用情况     NUMBER(1,0),
+    解聘日期     DATE,
+    解聘原因     VARCHAR2(160),
+    工人工种名称   SMALLINT,
+    备注       VARCHAR2(500)
+  );
+/*==============================================================*/
+/* Table: 惩处
+/*==============================================================*/
+CREATE TABLE 惩处
+  (
+    全局编号   VARCHAR2(64),
+    职工号    VARCHAR2(10),
+    最后修改时间 VARCHAR2(25),
+    惩处名称   VARCHAR2(80),
+    惩处原因   VARCHAR2(160),
+    惩处内容   VARCHAR2(160),
+    惩处单位   VARCHAR2(120),
+    惩处文号   VARCHAR2(48),
+    惩处日期   DATE,
+    惩处撤销日期 DATE,
+    惩处撤销文号 VARCHAR2(48),
+    惩处撤销原因 VARCHAR2(160),
+    备注     VARCHAR2(500)
+  );
+/*==============================================================*/
+/* Table: 指导博士后
+/*==============================================================*/
+CREATE TABLE 指导博士后
+  (
+    全局编号   VARCHAR2(64),
+    职工号    VARCHAR2(10),
+    最后修改时间 VARCHAR2(25),
+    博士后职工号 VARCHAR2(10),
+    流动站名称  VARCHAR2(400),
+    所在学院   SMALLINT,
+    研究项目   VARCHAR2(500),
+    进站时间   DATE,
+    进站学科   SMALLINT,
+    博士后类型  SMALLINT,
+    备注     VARCHAR2(500)
+  );
+/*==============================================================*/
+/* Table: 指导国内访问学者
+/*==============================================================*/
+CREATE TABLE 指导国内访问学者
+  (
+    全局编号   VARCHAR2(64),
+    职工号    VARCHAR2(10),
+    最后修改时间 VARCHAR2(25),
+    推荐单位   VARCHAR2(400),
+    接受单位   SMALLINT,
+    访问学者   VARCHAR2(10),
+    课题     VARCHAR2(500),
+    访学时间   DATE,
+    访学证书号  VARCHAR2(60),
+    备注     VARCHAR2(500)
+  );
+/*==============================================================*/
+/* Table: 来校前工作简历
+/*==============================================================*/
+CREATE TABLE 来校前工作简历
+  (
+    全局编号   VARCHAR2(64),
+    职工号    VARCHAR2(10),
+    最后修改时间 VARCHAR2(25),
+    起始年月   DATE,
+    截止年月   DATE,
+    所在单位名称 VARCHAR2(120),
+    从事工作内容 VARCHAR2(120),
+    曾任党政职务 SMALLINT,
+    曾任技术职务 SMALLINT,
+    证明人    VARCHAR2(60),
+    备注     VARCHAR2(500)
+  );
+/*==============================================================*/
+/* Table: 校内异动
+/*==============================================================*/
+CREATE TABLE 校内异动
+  (
+    全局编号       VARCHAR2(64),
+    职工号        VARCHAR2(10),
+    最后修改时间     VARCHAR2(25),
+    任职单位记录全局编号 VARCHAR2(64),
+    校内调出单位     SMALLINT,
+    校内调出所室名称   VARCHAR2(120),
+    校内调入单位     SMALLINT,
+    校内调入所室名称   VARCHAR2(120),
+    编制异动类别     NUMBER(1,0),
+    编制异动年月     DATE,
+    编制异动原因     VARCHAR2(160),
+    离岗年月       DATE,
+    离岗原因       NUMBER(1,0),
+    返岗年月       DATE,
+    病休起始年月     DATE,
+    病休诊断医院     VARCHAR2(120),
+    病休诊断证明     VARCHAR2(1200),
+    恢复工作年月     DATE,
+    康复诊断医院     VARCHAR2(120),
+    康复诊断证明     VARCHAR2(1200),
+    备注         VARCHAR2(500)
+  );
+/*==============================================================*/
+/* Table: 离校
+/*==============================================================*/
+CREATE TABLE 离校
+  (
+    全局编号   VARCHAR2(64),
+    职工号    VARCHAR2(10),
+    最后修改时间 VARCHAR2(25),
+    离校年月   DATE,
+    离校原因   SMALLINT,
+    离校去向   VARCHAR2(120),
+    备注     VARCHAR2(500)
+  );
+/*==============================================================*/
+/* Table: 考核
+/*==============================================================*/
+CREATE TABLE 考核
+  (
+    全局编号   VARCHAR2(64),
+    职工号    VARCHAR2(10),
+    最后修改时间 VARCHAR2(25),
+    考核时间   DATE,
+    考核类别   SMALLINT,
+    考核岗位   VARCHAR2(120),
+    考核内容   VARCHAR2(500),
+    考核等级   SMALLINT,
+    考核结果   SMALLINT,
+    备注     VARCHAR2(500)
+  );
+/*==============================================================*/
+/* Table: 联系方式
+/*==============================================================*/
+CREATE TABLE 联系方式
+  (
+    全局编号   VARCHAR2(64),
+    职工号    VARCHAR2(10),
+    最后修改时间 VARCHAR2(25),
+    单位电话   VARCHAR2(120),
+    家庭电话   VARCHAR2(120),
+    手机     VARCHAR2(120),
+    传真     VARCHAR2(120),
+    电子信箱   VARCHAR2(120),
+    主页地址   VARCHAR2(240),
+    通信地址   VARCHAR2(240),
+    家庭住址   VARCHAR2(120),
+    邮政编码   VARCHAR2(12),
+    备注     VARCHAR2(500)
+  );
+/*==============================================================*/
+/* Table: 聘用合同
+/*==============================================================*/
+CREATE TABLE 聘用合同
+  (
+    全局编号   VARCHAR2(64),
+    职工号    VARCHAR2(10),
+    最后修改时间 VARCHAR2(25),
+    合同类别   SMALLINT,
+    起聘日期   DATE,
+    终聘日期   DATE,
+    合同编号   VARCHAR2(200),
+    合同文件   VARCHAR2(200),
+    备注     VARCHAR2(500),
+    聘用岗位   VARCHAR2(32)
+  );
+/*==============================================================*/
+/* Table: 荣誉性奖励
+/*==============================================================*/
+CREATE TABLE 荣誉性奖励
+  (
+    全局编号   VARCHAR2(64),
+    职工号    VARCHAR2(10),
+    最后修改时间 VARCHAR2(25),
+    获奖项目   VARCHAR2(120),
+    奖励级别   SMALLINT,
+    获奖类别   NUMBER(1,0),
+    奖励方式   VARCHAR2(40),
+    奖励名称   VARCHAR2(120),
+    奖励名称分类 SMALLINT,
+    颁奖单位   VARCHAR2(120),
+    颁奖日期   SMALLINT,
+    荣誉称号   VARCHAR2(120),
+    获荣誉日期  DATE,
+    获奖角色   SMALLINT,
+    备注     VARCHAR2(500)
+  );
+/*==============================================================*/
 /* Table: DICT_人员类别代码                                       */
 /*==============================================================*/
 CREATE TABLE DICT_人员类别
   (
     人员类别代码 VARCHAR2(50),
     人员类别名称 VARCHAR2(50)
-  );
-/*==============================================================*/
-/* Table: 任职单位                                                  */
-/*==============================================================*/
-CREATE TABLE 任职单位
-  (
-    全局编号   VARCHAR2(64),
-    职工号    VARCHAR2(10) NOT NULL,
-    最后修改时间 VARCHAR2(25),
-    人事关系所在单位 NUMBER(1, 0),
-    调动时间 DATE,
-    单位   VARCHAR2(255) NOT NULL,
-    岗位   VARCHAR2(100),
-    科室   VARCHAR2(100),
-    学科   VARCHAR2(255),
-    专业   VARCHAR2(255),
-    科室名称 VARCHAR2(120),
-    备注   VARCHAR2(500),
-    从事学科 SMALLINT,
-    从事专业 VARCHAR2(6),
-    主要岗位 SMALLINT,
-    兼职岗位 SMALLINT,
-    CONSTRAINT 任职单位 PRIMARY KEY (职工号, 单位)
   );
 /*==============================================================*/
 /* Table: DICT_党政职务                                             */
@@ -362,8 +681,7 @@ CREATE TABLE DICT_专业
 /*==============================================================*/
 CREATE TABLE DICT_健康状况
   (
-    代码    VARCHAR2(50)
-    健康状况 VARCHAR2(50) NOT NULL,
+    代码 VARCHAR2(50) 健康状况 VARCHAR2(50) NOT NULL,
     CONSTRAINT PK_DICT_健康状况 PRIMARY KEY (健康状况)
   );
 /*==============================================================*/
