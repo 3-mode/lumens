6d4897b356a2b94737ffd0c7883f141781950ab4
shaofeng.wang@outlook.com <>
2014-03-14 15:35:50 +0800

    add route in application entry js

--- lumens-web/src/main/webapp/app/lumens.js
+++ lumens-web/src/main/webapp/app/lumens.js
@@ -1,4 +1,19 @@
 /* 
  * Application
  */
-Lumens.$ngApp = angular.module("lumens-app", ["lumens-directives", "lumens-controllers"]);
+Lumens.$ngApp = angular.module("lumens-app", ["lumens-directives", "lumens-controllers", "ngRoute"]);
+Lumens.$ngApp.config(function($routeProvider) {
+    $routeProvider
+    .when("/id-dashboard-view", {
+        template: "",
+        controller: "DashboardViewCtrl"
+    })
+    .when("/id-management-view", {
+        template: "",
+        controller: "ManageViewCtrl"
+    })
+    .when("/id-desinger-view", {
+        template: "",
+        controller: "DesignViewCtrl"
+    });
+});
