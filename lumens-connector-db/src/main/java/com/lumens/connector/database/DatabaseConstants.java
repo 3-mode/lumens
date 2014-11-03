ca5dc950e3dfeb5dfcdb2bb685760b0f0f8c34f0
xiaoxin <whiskeyfly@163.com>
2014-11-03 08:43:23 +0800

    Update sqlserver constant

--- lumens-connector-db/src/main/java/com/lumens/connector/database/DatabaseConstants.java
+++ lumens-connector-db/src/main/java/com/lumens/connector/database/DatabaseConstants.java
@@ -15,14 +15,14 @@ public interface DatabaseConstants {
     public static final String SESSION_ALTER = "SessionAlter";
     
     // Connector constant
-    public static String CONST_CNTR_SQLSERVER_CLASS = "oracle.jdbc.OracleDriver";
+    public static String CONST_CNTR_SQLSERVER_CLASS = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
     public static String CONST_CNTR_SQLSERVER_TYPE = "type";
     public static String CONST_CNTR_SQLSERVER_DESCRIPTION = "description";
     public static String CONST_CNTR_SQLSERVER_DATA_LENGTH = "data_length";
     public static String CONST_CNTR_SQLSERVER_DATA_TYPE = "data_type";
     // SQL Server SQL to query tables information
-    public static String CONST_CNTR_SQLSERVER_TABLENAMES = "select t.table_name,t.comments, t.TABLE_TYPE from user_tab_comments t";
-    public static String CONST_CNTR_SQLSERVER_TABLECOLUMNS = "select t.COLUMN_NAME, t.DATA_TYPE, t.DATA_LENGTH from user_tab_columns t where t.TABLE_NAME = '%s'";
+    public static String CONST_CNTR_SQLSERVER_TABLENAMES = "select t.name from master..SysDatabases t;";
+    public static String CONST_CNTR_SQLSERVER_TABLECOLUMNS = "select t.'%s' from test..SysObjects t where xtype='u'";
     // SQL Server data types
     public static String CONST_CNTR_SQLSERVER_CHAR = "CHAR";
     public static String CONST_CNTR_SQLSERVER_VARCHAR2 = "VARCHAR2";
