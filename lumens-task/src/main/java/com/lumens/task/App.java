34a8a33268e06e5893b369022d4a9945b606c589
shaofeng.wang@outlook.com <>
2014-11-07 08:17:48 +0800

    add task database setting code in unit test

--- lumens-task/src/main/java/com/lumens/task/App.java
+++ lumens-task/src/main/java/com/lumens/task/App.java
@@ -1,13 +1,8 @@
 package com.lumens.task;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 }
--- lumens-task/src/test/java/com/lumens/task/TaskManagementTest.java
+++ lumens-task/src/test/java/com/lumens/task/TaskManagementTest.java
@@ -1,5 +1,6 @@
 package com.lumens.task;
 
+import java.util.Properties;
 import org.quartz.JobBuilder;
 import org.quartz.JobDetail;
 import org.quartz.Scheduler;
@@ -14,8 +15,27 @@ public class TaskManagementTest {
 
     @Test
     public void testScheduler() throws Exception {
+        Properties properties = new Properties();
+        properties.put("org.quartz.scheduler.instanceName", "TestScheduler");
+        properties.put("org.quartz.scheduler.instanceId", "AUTO");
+        properties.put("org.quartz.scheduler.skipUpdateCheck", "true");
+        properties.put("org.quartz.threadPool.class", "org.quartz.simpl.SimpleThreadPool");
+        properties.put("org.quartz.threadPool.threadCount", "12");
+        properties.put("org.quartz.threadPool.threadPriority", "5");
+        properties.put("org.quartz.jobStore.misfireThreshold", "10000");
+        properties.put("org.quartz.jobStore.class", "org.quartz.impl.jdbcjobstore.JobStoreTX");
+        properties.put("org.quartz.jobStore.driverDelegateClass", "org.quartz.impl.jdbcjobstore.StdJDBCDelegate");
+        properties.put("org.quartz.jobStore.useProperties", "true");
+        properties.put("org.quartz.jobStore.dataSource", "lumensDS");
+        properties.put("org.quartz.jobStore.tablePrefix", "QRTZ_");
+        properties.put("org.quartz.jobStore.isClustered", "false");
+        properties.put("org.quartz.dataSource.lumensDS.driver", "org.apache.derby.jdbc.ClientDriver");
+        properties.put("org.quartz.dataSource.lumensDS.URL", "jdbc:derby://localhost:1527/lumens");
+        properties.put("org.quartz.dataSource.lumensDS.user", "lumens");
+        properties.put("org.quartz.dataSource.lumensDS.password", "lumens");
+        properties.put("org.quartz.dataSource.lumensDS.maxConnections", "5");
 
-        SchedulerFactory sf = new StdSchedulerFactory();
+        SchedulerFactory sf = new StdSchedulerFactory(properties);
         Scheduler sched = sf.getScheduler();
 
         JobDetail job = JobBuilder.newJob(SimpleJob.class)
