0afd1275c7585ac2e19c6bacd1726a184b6f25d5
Xiaoxin(whiskeyfly@163.com) <whiskeyfly@163.com>
2014-11-14 08:47:34 +0800

    Change unit test name and junit version from 3.81 to 4.11

---
+++ lumens-connector-xml/src/test/java/com/lumens/connector/xml/ConnectorTest.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright Lumens Team, Inc. All Rights Reserved.
+ */
+
+package com.lumens.connector.xml;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ *
+ * @author Xiaoxin(whiskeyfly@163.com)
+ */
+public class ConnectorTest 
+{
+    @Test
+    public void testSqlServerConnector() {
+        
+    }    
+}
