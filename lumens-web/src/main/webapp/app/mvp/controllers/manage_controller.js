5a99190b625e814e69030abb7ef723d7d3349074
shaofeng.wang@outlook.com <>
2014-11-17 01:47:16 +0800

    design the job manage UI, add datetime plugin, add job sql table.

--- lumens-web/src/main/webapp/app/mvp/controllers/manage_controller.js
+++ lumens-web/src/main/webapp/app/mvp/controllers/manage_controller.js
@@ -3,7 +3,7 @@
  */
 
 Lumens.controllers
-.controller("ManageViewCtrl", function ($scope, $route, $compile, JobList, SyncGet) {
+.controller("ManageViewCtrl", function ($scope, $route, $compile, JobList, SyncGet, ProjectList) {
     LumensLog.log("in ManageViewCtrl");
     Lumens.system.navToolbar.active($route.current.$$route.originalPath.substring(1));
     Lumens.system.switchTo(Lumens.system.AngularJSView);
@@ -14,72 +14,72 @@ Lumens.controllers
     $scope.job_config_template = "app/templates/manage/job_config_tmpl.html";
     $scope.job_create_template = "app/templates/manage/job_create_modal_tmpl.html";
     $scope.job_function_tool_template = "app/templates/manage/job_function_tool_tmpl.html";
+    $scope.job_add_proj_manage_template = "app/templates/manage/job_add_proj_manage_tmpl.html";
     $scope.jobListHolder = new Lumens.List($("#jobList"));
     $scope.job_config = SyncGet.get("app/config/json/job_config.json", "text/json");
     $scope.job = {};
+    var jobManagementHolder = $("#jobManagementHolder");
+    $(window).resize(function (e) {
+        if (e && e.target !== this)
+            return;
+        jobManagementHolder.trigger("resize");
+    })
+    $scope.jobManagePanel = new Lumens.ResizableVSplitLayoutExt(jobManagementHolder)
+    .configure({
+        mode: "vertical",
+        useRatio: true,
+        part1Size: "40%"
+    });
+    $scope.jobManagePanel.getPart1Element().append($compile('<div ng-include="job_list_template" class="lumens-scroll-panel"></div>')($scope));
+    $scope.jobManagePanel.getPart2Element().append($compile('<div ng-include="job_config_template" class="lumens-scroll-panel"></div>')($scope));
     // Job command bar function
     $scope.onCommand = function (id_btn) {
         LumensLog.log("in Job onCommand");
     };
+    $scope.openJobDetail = function (index, job_item, evt) {
+        $scope.selectJobIndex = index;
+        LumensLog.log("Event", index, job_item, evt);
+        $scope.job = job_item;
+    };
+    $scope.selectProjItem = function (index) {
+        $scope.selectProjIndex = index;
+    };
+    $scope.selectAddedProject = function (index) {
+        $scope.selectAddedProjIndex = index;
+    };
+    $scope.AddManageProject = function () {
+        ProjectList.get(function (projectsResponse) {
+            $scope.projects = projectsResponse.content.project;
+        });
+    };
+    $scope.addProject = function () {
+        $("#projectListAddModal").find("#projectLoading").show();
+    }
     // Create job function
     $scope.createJob = function () {
         LumensLog.log("in createJob");
         LumensLog.log("Job", $scope.job);
         $scope.job.id = $.currentTime();
-        var itemContentHolder;
-        $scope.jobListHolder.addItem({
-            IdList: [
-                $scope.job.id
-            ],
-            titleList: [
-                $scope.job.name + " [" + $scope.job.id + "]"
-            ],
-            buildContent: function (itemContent) {
-                itemContent.append($compile('<div ng-include="job_config_template" />')($scope));
-                itemContentHolder = itemContent;
-            }
-        }, 0, true);
-        itemContentHolder.find('#' + $scope.job.id).datetimepicker({
-            language: 'en',
-            weekStart: 1,
-            todayBtn: 1,
-            autoclose: 1,
-            todayHighlight: 1,
-            startView: 1,
-            minView: 0,
-            maxView: 1,
-            forceParse: 0
-        });
         $scope.job = {};
     };
     JobList.get(function (result) {
         $scope.jobs = result.jobs;
         LumensLog.log(result);
-        for (var i = 0; i < result.jobs.length; ++i) {
-            var itemContentHolder;
-            $scope.jobListHolder.addItem({
-                IdList: [
-                    result.jobs[i].id
-                ],
-                titleList: [
-                    result.jobs[i].name + " [" + result.jobs[i].id + "]"
-                ],
-                buildContent: function (itemContent) {
-                    itemContent.append($compile('<div ng-include="job_config_template" />')($scope));
-                    itemContentHolder = itemContent;
-                }
-            }, i);
-            itemContentHolder.find('#' + result.jobs[i].id).datetimepicker({
-                language: 'en',
-                weekStart: 1,
-                todayBtn: 1,
-                autoclose: 1,
-                todayHighlight: 1,
-                startView: 1,
-                minView: 0,
-                maxView: 1,
-                forceParse: 0
-            });
-        }//*/
+    });
+})
+.controller("JobConfigInitCtrl", function ($scope) {
+    $scope.jobManagePanel.getPart2Element().find('.form_time')
+    .datetimepicker({
+        container: $scope.jobManagePanel.getPart2Element().find("#jobCofnig"),
+        language: 'en',
+        weekStart: 1,
+        todayBtn: 1,
+        autoclose: 1,
+        todayHighlight: 1,
+        startView: 1,
+        minView: 0,
+        maxView: 1,
+        forceParse: 0,
+        pickerPosition: "bottom-left"
     });
 });
