ce7d7dec7d80a9caf2f56144a21f3f4a871649d7
shaofeng-wang <shaofeng.wang@outlook.com>
2014-12-03 21:52:30 +0800

    add islastchunk and change to transform mapper

---
+++ lumens-engine/src/main/java/com/lumens/engine/core/TransferEngine.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright Lumens Team, Inc. All Rights Reserved.
+ */
+package com.lumens.engine.core;
+
+/**
+ *
+ * @author Shaofeng Wang <shaofeng.wang@outlook.com>
+ */
+public class TransferEngine {
+    public void execute() {
+
+    }
+}
