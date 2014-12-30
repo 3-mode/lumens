b462db62ef5dcd63b07a77625ba41c824bb48872
shaofeng-wang <shaofeng.wang@outlook.com>
2014-12-30 13:41:24 +0800

    Backup boostrap nav

--- lumens-web/src/main/webapp/app/mvp/controllers/main_controller.js
+++ lumens-web/src/main/webapp/app/mvp/controllers/main_controller.js
@@ -4,6 +4,8 @@
 
 Lumens.controllers.controller("MainViewCtrl", function ($scope, $route, $http, $compile) {
     // Set the default page view as dashboard view
+    $scope.templates_topbar_url = "app/templates/topbar.html";
+    /*
     Lumens.system.rootLayout = new Lumens.RootLayout($('#id-main-view')).configure();
     Lumens.system.theLayout = new Lumens.SplitLayout(Lumens.system.rootLayout).configure({
         mode: "vertical",
@@ -11,5 +13,5 @@ Lumens.controllers.controller("MainViewCtrl", function ($scope, $route, $http, $
     });
     Lumens.system.sysHeader = new Lumens.Header(Lumens.system.theLayout.getPart1Element()).setSysTitle("LUMENS");
     Lumens.system.navToolbar = new Lumens.NavToolbar(Lumens.system.sysHeader.getElement()).configure(Lumens.SysToolbar_Config);
-    Lumens.system.theLayout.getPart2Element().append($compile('<div ng-view id="theWorkspaceContainer" style="width: 100%;"/>')($scope));
+    Lumens.system.theLayout.getPart2Element().append($compile('<div ng-view id="theWorkspaceContainer" style="width: 100%;"/>')($scope));//*/
 });
