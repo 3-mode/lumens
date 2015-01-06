559a6ee53710d9240ec6155bbb6919ea9124c2b7
shaofeng-wang <shaofeng.wang@outlook.com>
2015-01-07 01:10:39 +0800

    Remove import

--- lumens-connector-text/src/main/java/com/lumens/connector/txt/TextClient.java
+++ lumens-connector-text/src/main/java/com/lumens/connector/txt/TextClient.java
@@ -18,11 +18,6 @@ import java.util.List;
 import java.util.ArrayList;
 import java.util.Map;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
