5fdcf55cc263da9afb43531f28b02a254f338236
Xiaoxin(whiskeyfly@163.com) <whiskeyfly@163.com>
2015-01-14 09:08:59 +0800

    Add trigger

---
+++ lumens-scheduler/src/main/java/com/lumens/lumens/scheduler/DefaultTrigger.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.lumens.lumens.scheduler;
+
+import java.util.Date;
+import static org.quartz.TriggerBuilder.*;
+import static org.quartz.SimpleScheduleBuilder.*;
+import static org.quartz.DateBuilder.*;
+
+/**
+ *
+ * @author xiaoxiao
+ */
+public class DefaultTrigger implements Trigger{
+    private Date startTime;
+    private Date endTime;
+    private int repeatCount;
+    private int interval;
+    
+    public DefaultTrigger(Date startTime, Date endTime, int repeatCount, int interval){
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.repeatCount = repeatCount;
+        this.interval = interval;
+    }
+    
+    public DefaultTrigger(String cron){
+        
+    }
+    
+    public int getRepeatCount(){
+        return repeatCount;
+    }
+    
+    public Date getStartTime(){
+        return startTime;
+    }
+    
+    public Date getEndTime(){
+        return endTime;
+    }
+    
+    // In millisecond
+    public int getRepeatInterval(){
+        return interval;
+    }
+}
---
+++ lumens-scheduler/src/main/java/com/lumens/lumens/scheduler/Trigger.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright Lumens Team, Inc. All Rights Reserved.
+ */
+
+package com.lumens.lumens.scheduler;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Xiaoxin(whiskeyfly@163.com)
+ */
+public interface Trigger {
+    public int getRepeatCount();
+    public Date getStartTime();
+    public Date getEndTime();
+    public int getRepeatInterval();
+}
