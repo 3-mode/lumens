5c2cb245c81b7bd2dc78b5df172640aadab29cea
shaofeng-wang <shaofeng.wang@outlook.com>
2015-01-15 02:06:49 +0800

    Make the port as parameter

--- dist/lumens/server.bat
+++ dist/lumens/server.bat
@@ -3,5 +3,5 @@ set "MY_HOME=%CD%"
 set "CLASSPATH=%MY_HOME%\module\shared;%MY_HOME%\module\web;%MY_HOME%\server;%MY_HOME%\module\server\lib;%CLASSPATH%"
 set "JAVA_HOME=C:\Sun\Java\jdk1.7.0_21"
 set "PATH=%JAVA_HOME%\bin;%PATH%"
-java -classpath %CLASSPATH% -Dlumens.web=module/web -Dlumens.addin=addin -jar module\server\lumens-server-1.0.jar
+java -classpath %CLASSPATH% -Dlumens.base=%MY_HOME% -Dlumens.web=%MY_HOME%\module\web -Dlumens.port=9090 -jar module\server\lumens-server-1.0.jar
 pause
