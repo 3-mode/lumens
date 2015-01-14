cd19c1f239e822c82db37eb2ceb9d9fa25aeaf25
shaofeng-wang <shaofeng.wang@outlook.com>
2015-01-15 01:17:34 +0800

    Refine sqlserver code Move sqlserver code to lumens-connector-db Remove session alter because it is for oracle

--- lumens-connector-sqlserver/src/main/java/com/lumens/connector/database/client/sqlserver/service/Activator.java
+++ lumens-connector-sqlserver/src/main/java/com/lumens/connector/database/client/sqlserver/service/Activator.java
@@ -1,13 +1,12 @@
 /*
  * Copyright Lumens Team, Inc. All Rights Reserved.
  */
-
 package com.lumens.connector.database.client.sqlserver.service;
 
-import com.lumens.connector.database.client.sqlserver.SqlServerConnectorFactory;
 import com.lumens.addin.AddinActivator;
 import com.lumens.addin.AddinContext;
 import com.lumens.connector.ConnectorFactory;
+import com.lumens.connector.database.sqlserver.SQLServerConnectorFactory;
 import com.lumens.descriptor.DescriptorUtils;
 
 /**
@@ -21,7 +20,7 @@ public class Activator implements AddinActivator {
     @Override
     public void start(AddinContext context) {
         addinContext = context;
-        ConnectorFactory factory = new SqlServerConnectorFactory();
+        ConnectorFactory factory = new SQLServerConnectorFactory();
         addinContext.registerService(factory.getComponentType(), factory, DescriptorUtils.processDescriptor(Activator.class, "sqlserver", factory.getComponentType()));
     }
 
