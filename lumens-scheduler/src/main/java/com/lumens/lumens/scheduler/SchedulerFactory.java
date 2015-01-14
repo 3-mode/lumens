a19680cc4cd3337d0f42cf573d943e9e8224e070
Xiaoxin(whiskeyfly@163.com) <whiskeyfly@163.com>
2015-01-15 00:03:12 +0800

    Add SchedulerFactory

---
+++ lumens-scheduler/src/main/java/com/lumens/lumens/scheduler/SchedulerFactory.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright Lumens Team, Inc. All Rights Reserved.
+ */
+package com.lumens.lumens.scheduler;
+
+import com.lumens.lumens.scheduler.impl.DefaultScheduler;
+
+/**
+ *
+ * @author Xiaoxin(whiskeyfly@163.com)
+ */
+public class SchedulerFactory {
+    private static final class SchedulerFactoryHolder{
+        private static final SchedulerFactory instance = new SchedulerFactory();
+    }
+    
+    public static SchedulerFactory get() {
+        return SchedulerFactoryHolder.instance;
+    }
+
+    private SchedulerFactory(){        
+    }
+    
+    public JobScheduler createScheduler() {        
+        return new DefaultScheduler();
+    }
+}
