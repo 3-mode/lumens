6b3a22df5a5f0b565ad2d08d328a82f7a5f989a6
Xiaoxin(whiskeyfly@163.com) <whiskeyfly@163.com>
2015-01-14 09:08:49 +0800

    Add cronbuilder

---
+++ lumens-scheduler/src/main/java/com/lumens/lumens/scheduler/CronBuilder.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright Lumens Team, Inc. All Rights Reserved.
+ */
+
+package com.lumens.lumens.scheduler;
+
+/**
+ *
+ * @author Xiaoxin(whiskeyfly@163.com)
+ */
+public class CronBuilder {
+    private long second;
+    private long minute;
+    private long hour;
+    private long dayOfMonth;
+    private long dayOfWeek;
+    private long month;
+    private long year;  // optional
+    
+    public CronBuilder(long hour, long minute, long second, long month, long dayOfMonth, long dayOfWeek, long year){
+        this.hour = hour;
+        this.minute = minute;
+        this.second = second;
+        this.month = month;
+        this.dayOfMonth = dayOfMonth;
+        this.dayOfWeek = dayOfWeek;
+        this.year = year;
+    }
+    
+    public String getCronExpression(){
+        return null;
+    }
+}
