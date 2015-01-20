230595df27f032d650d0e0965ec38a6c98ca8e0c
xiaoxin <whiskeyfly@163.com>
2015-01-20 08:50:36 +0800

    Add relation db access interface

---
+++ lumens-sysdb/src/main/java/com/lumens/sysdb/entity/Relation.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright Lumens Team, Inc. All Rights Reserved.
+ */
+package com.lumens.sysdb.entity;
+
+import com.lumens.sysdb.Column;
+import com.lumens.sysdb.Table;
+
+/**
+ *
+ * @author Xiaoxin(whiskeyfly@163.com)
+ */
+@Table(name = "LUNS_PROJECT")
+public class Relation {
+
+    @Column(name = "job_id")
+    public long jobId;
+    @Column(name = "project_id")
+    public long projectId;
+
+    public Relation(long jobId, long projectId) {
+        this.jobId = jobId;
+        this.projectId = projectId;
+    }
+
+    public Relation() {
+    }
+}
