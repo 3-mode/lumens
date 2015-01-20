7db5a399ee5fbba7b7d73c02c70b8626218e3ce9
xiaoxin <whiskeyfly@163.com>
2015-01-21 00:23:18 +0800

    Add jobDao factory

--- lumens-sysdb/src/main/java/com/lumens/sysdb/DAOFactory.java
+++ lumens-sysdb/src/main/java/com/lumens/sysdb/DAOFactory.java
@@ -2,6 +2,8 @@ package com.lumens.sysdb;
 
 import com.lumens.sysdb.dao.InOutLogDAO;
 import com.lumens.sysdb.dao.ProjectDAO;
+import com.lumens.sysdb.dao.JobDAO;
+import com.lumens.sysdb.dao.RelationDAO;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -16,7 +18,12 @@ public class DAOFactory {
     public static ProjectDAO getProjectDAO() {
         return (ProjectDAO) context.getBean("projectDAO");
     }
-
+    public static JobDAO getJobDAO() {
+        return (JobDAO) context.getBean("JobDAO");
+    }
+    public static RelationDAO getRelationDAO() {
+        return (RelationDAO) context.getBean("RelationDAO");
+    }    
     public static InOutLogDAO getInOutLogDAO() {
         return (InOutLogDAO) context.getBean("inputLogDAO");
     }
