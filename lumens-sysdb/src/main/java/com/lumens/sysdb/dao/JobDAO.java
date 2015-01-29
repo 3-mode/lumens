4cdc45b2805e7ee8c1b928ef3af5d9b5a239f8b7
shaofeng-wang <shaofeng.wang@outlook.com>
2015-01-29 13:07:50 +0800

    Make Job and Relation DAO more simple

--- lumens-sysdb/src/main/java/com/lumens/sysdb/dao/JobDAO.java
+++ lumens-sysdb/src/main/java/com/lumens/sysdb/dao/JobDAO.java
@@ -4,20 +4,7 @@
 package com.lumens.sysdb.dao;
 
 import com.lumens.sysdb.entity.Job;
-import com.lumens.sysdb.EntityFactory;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Timestamp;
-import java.util.ArrayList;
 import java.util.List;
-import org.springframework.dao.DataAccessException;
-import org.springframework.jdbc.core.PreparedStatementCallback;
-import org.springframework.jdbc.core.PreparedStatementCreator;
-import org.springframework.jdbc.core.RowCallbackHandler;
-import org.springframework.transaction.TransactionStatus;
-import org.springframework.transaction.support.TransactionCallbackWithoutResult;
 
 /**
  *
@@ -27,119 +14,28 @@ public class JobDAO extends BaseDAO {
     public int getTotal() {
         return super.getTotal("JobDAO/total");
     }
-    
+
     public long create(final Job job) {
-        transactionTemplate.execute(new TransactionCallbackWithoutResult() {
-            @Override
-            protected void doInTransactionWithoutResult(TransactionStatus paramTransactionStatus) {
-                try {
-                    jdbcTemplate.execute(new PreparedStatementCreator() {
-                        @Override
-                        public PreparedStatement createPreparedStatement(Connection cnctn) throws SQLException {
-                            return cnctn.prepareStatement(sqlManager.getSQL("JobDAO/CreateJob"));
-                        }
-                    }, new PreparedStatementCallback<Boolean>() {
-                        @Override
-                        public Boolean doInPreparedStatement(PreparedStatement ps) throws SQLException, DataAccessException {
-                            ps.setLong(1, job.id);
-                            ps.setString(2, job.name);
-                            ps.setString(3, job.description);
-                            ps.setInt(4, job.repeatCount);
-                            ps.setInt(5, job.interval);
-                            ps.setTimestamp(6, job.startTime);
-                            ps.setTimestamp(7, job.endTime);
-                            return ps.execute();
-                        }
-                    });
-                } catch (Exception e) {
-                    //use this to rollback exception in case of exception
-                    paramTransactionStatus.setRollbackOnly();
-                    throw new RuntimeException(e);
-                }
-            }
-        });
+        simplePrepareStatTransactionExecute(sqlManager.getSQL("JobDAO/CreateJob"), job.id, job.name, job.description, job.repeatCount, job.interval, job.startTime, job.endTime);
         return job.id;
     }
 
     public long update(final Job job) {
-        transactionTemplate.execute(new TransactionCallbackWithoutResult() {
-            @Override
-            protected void doInTransactionWithoutResult(TransactionStatus paramTransactionStatus) {
-                try {
-                    jdbcTemplate.execute(new PreparedStatementCreator() {
-                        @Override
-                        public PreparedStatement createPreparedStatement(Connection cnctn) throws SQLException {
-                            return cnctn.prepareStatement(sqlManager.getSQL("JobDAO/UpdateJob"));
-                        }
-                    }, new PreparedStatementCallback<Boolean>() {
-                        @Override
-                        public Boolean doInPreparedStatement(PreparedStatement ps) throws SQLException, DataAccessException {                            
-                            ps.setString(1, job.name);
-                            ps.setString(2, job.description);
-                            ps.setInt(3, job.repeatCount);
-                            ps.setInt(4, job.interval);
-                            ps.setTimestamp(5, job.startTime);
-                            ps.setTimestamp(6, job.endTime);
-                            ps.setLong(7, job.id);
-                            return ps.execute();
-                        }
-                    });
-                } catch (Exception e) {
-                    //use this to rollback exception in case of exception
-                    paramTransactionStatus.setRollbackOnly();
-                    throw new RuntimeException(e);
-                }
-            }
-        });
+        simplePrepareStatTransactionExecute(sqlManager.getSQL("JobDAO/UpdateJob"), job.id, job.name, job.description, job.repeatCount, job.interval, job.startTime, job.endTime);
         return job.id;
     }
 
     public long delete(final long jobId) {
-        transactionTemplate.execute(new TransactionCallbackWithoutResult() {
-            @Override
-            protected void doInTransactionWithoutResult(TransactionStatus paramTransactionStatus) {
-                try {
-                    jdbcTemplate.execute(new PreparedStatementCreator() {
-                        @Override
-                        public PreparedStatement createPreparedStatement(Connection cnctn) throws SQLException {
-                            return cnctn.prepareStatement(sqlManager.getSQL("JobDAO/DeleteJob"));
-                        }
-                    }, new PreparedStatementCallback<Boolean>() {
-                        @Override
-                        public Boolean doInPreparedStatement(PreparedStatement ps) throws SQLException, DataAccessException {
-                            ps.setLong(1, jobId);
-                            return ps.execute();
-                        }
-                    });
-                } catch (Exception e) {
-                    //use this to rollback exception in case of exception
-                    paramTransactionStatus.setRollbackOnly();
-                    throw new RuntimeException(e);
-                }
-            }
-        });
+        simplePrepareStatTransactionExecute(sqlManager.getSQL("JobDAO/DeleteJob"), jobId);
         return jobId;
-    }  
-    
+    }
+
     public Job getJob(long jobId) {
-        final List<Job> pList = new ArrayList<>();
-        jdbcTemplate.query(sqlManager.getSQL("JobDAO/FindJob", jobId), new RowCallbackHandler() {
-            @Override
-            public void processRow(ResultSet rs) throws SQLException {
-                pList.add(EntityFactory.createEntity(Job.class, rs));
-            }
-        });
+        List<Job> pList = simpleQuery(sqlManager.getSQL("JobDAO/FindJob", jobId), Job.class);
         return pList.size() > 0 ? pList.get(0) : null;
-    }    
-    
+    }
+
     public List<Job> getAllJob() {
-        final List<Job> pList = new ArrayList<>();
-        jdbcTemplate.query(sqlManager.getSQL("JobDAO/AllJob"), new RowCallbackHandler() {
-            @Override
-            public void processRow(ResultSet rs) throws SQLException {
-                pList.add(EntityFactory.createEntity(Job.class, rs));
-            }
-        });
-        return pList;
-    }    
+        return simpleQuery(sqlManager.getSQL("JobDAO/AllJob"), Job.class);
+    }
 }
--- lumens-sysdb/src/main/java/com/lumens/sysdb/dao/RelationDAO.java
+++ lumens-sysdb/src/main/java/com/lumens/sysdb/dao/RelationDAO.java
@@ -4,16 +4,7 @@
 package com.lumens.sysdb.dao;
 
 import com.lumens.sysdb.entity.Relation;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
-import org.springframework.dao.DataAccessException;
-import org.springframework.jdbc.core.PreparedStatementCallback;
-import org.springframework.jdbc.core.PreparedStatementCreator;
-import org.springframework.transaction.TransactionStatus;
-import org.springframework.transaction.support.TransactionCallbackWithoutResult;
 
 /**
  *
@@ -22,66 +13,18 @@ import org.springframework.transaction.support.TransactionCallbackWithoutResult;
 public class RelationDAO extends BaseDAO {
 
     public void create(final long jobId, final long projectId) {
-        transactionTemplate.execute(new TransactionCallbackWithoutResult() {
-            @Override
-            protected void doInTransactionWithoutResult(TransactionStatus paramTransactionStatus) {
-                try {
-                    jdbcTemplate.execute(new PreparedStatementCreator() {
-                        @Override
-                        public PreparedStatement createPreparedStatement(Connection cnctn) throws SQLException {
-                            return cnctn.prepareStatement(sqlManager.getSQL("RelationDAO/CreateRelation"));
-                        }
-                    }, new PreparedStatementCallback<Boolean>() {
-                        @Override
-                        public Boolean doInPreparedStatement(PreparedStatement ps) throws SQLException, DataAccessException {
-                            ps.setLong(1, jobId);
-                            ps.setLong(2, projectId);
-                            return ps.execute();
-                        }
-                    });
-                } catch (Exception e) {
-                    //use this to rollback exception in case of exception
-                    paramTransactionStatus.setRollbackOnly();
-                    throw new RuntimeException(e);
-                }
-            }
-        });
+        simplePrepareStatTransactionExecute(sqlManager.getSQL("RelationDAO/CreateRelation"), jobId, projectId);
     }
 
     public List<Relation> getAllRelation(long jobId) {
-        final List<Relation> pList = new ArrayList<>();
-        jdbcTemplate.execute(sqlManager.getSQL("RelationDAO/AllRelation", jobId));
-        return pList;
+        return simpleQuery(sqlManager.getSQL("RelationDAO/AllRelation", jobId), Relation.class);
     }
 
     public void deleteAllRelation(long jobId) {
-        this.simpleTransactionExecute(sqlManager.getSQL("RelationDAO/DeleteAllRelation", jobId));
+        simpleTransactionExecute(sqlManager.getSQL("RelationDAO/DeleteAllRelation", jobId));
     }
 
     public void delete(final long jobId, final long projectId) {
-        transactionTemplate.execute(new TransactionCallbackWithoutResult() {
-            @Override
-            protected void doInTransactionWithoutResult(TransactionStatus paramTransactionStatus) {
-                try {
-                    jdbcTemplate.execute(new PreparedStatementCreator() {
-                        @Override
-                        public PreparedStatement createPreparedStatement(Connection cnctn) throws SQLException {
-                            return cnctn.prepareStatement(sqlManager.getSQL("RelationDAO/DeleteRelation"));
-                        }
-                    }, new PreparedStatementCallback<Boolean>() {
-                        @Override
-                        public Boolean doInPreparedStatement(PreparedStatement ps) throws SQLException, DataAccessException {
-                            ps.setLong(1, jobId);
-                            ps.setLong(2, projectId);
-                            return ps.execute();
-                        }
-                    });
-                } catch (Exception e) {
-                    //use this to rollback exception in case of exception
-                    paramTransactionStatus.setRollbackOnly();
-                    throw new RuntimeException(e);
-                }
-            }
-        });
+        simplePrepareStatTransactionExecute(sqlManager.getSQL("RelationDAO/DeleteRelation"), jobId, projectId);
     }
 }
