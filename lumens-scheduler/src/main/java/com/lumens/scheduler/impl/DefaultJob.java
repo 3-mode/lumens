88a5277942b3e2caa3fb5e8ffcfc26335e16787a
shaofeng-wang <shaofeng.wang@outlook.com>
2015-01-29 18:05:06 +0800

    Merge with remote

--- lumens-scheduler/src/main/java/com/lumens/scheduler/impl/DefaultJob.java
+++ lumens-scheduler/src/main/java/com/lumens/scheduler/impl/DefaultJob.java
@@ -3,26 +3,14 @@
  */
 package com.lumens.scheduler.impl;
 
-import com.lumens.engine.TransformProject;
-import com.lumens.engine.handler.ResultHandler;
-import com.lumens.engine.run.SequenceTransformExecuteJob;
-import com.lumens.engine.serializer.ProjectSerializer;
-import com.lumens.sysdb.DAOFactory;
-import com.lumens.sysdb.dao.ProjectDAO;
-import com.lumens.sysdb.entity.Project;
-import java.io.ByteArrayInputStream;
 import java.util.ArrayList;
-import org.quartz.Job;
-import org.quartz.JobExecutionContext;
-import org.quartz.JobExecutionException;
-import org.quartz.JobDetail;
 import java.util.List;
 
 /**
  *
  * @author Xiaoxin(whiskeyfly@163.com)
  */
-public class DefaultJob implements Job {
+public class DefaultJob  {
     private final long jobId;
     private final String name;
     private final String description;
@@ -61,17 +49,5 @@ public class DefaultJob implements Job {
 
     public List<Long> getProjectList() {
         return projectIdList;
-    }
-
-    public void execute(JobExecutionContext jec) throws JobExecutionException {
-        JobDetail job = jec.getJobDetail();
-        String jobId = job.getKey().getGroup();
-        String projectId = job.getKey().getName();
-        
-        System.out.println("Executing Job: " + jobId + " Project:" + projectId);
-        
-        String projectData = jec.getMergedJobDataMap().getString("Project");
-        System.out.println("Project date = " + projectData);        
-        // TODO: sent project id to Application global context to execute, or execute here
     }
 }
