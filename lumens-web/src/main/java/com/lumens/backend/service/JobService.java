47b16a649d88ac8b785373f7b261a69172698dda
shaofeng-wang <shaofeng.wang@outlook.com>
2015-02-05 18:13:45 +0800

    Add REST for get all job and related projects

--- lumens-web/src/main/java/com/lumens/backend/service/JobService.java
+++ lumens-web/src/main/java/com/lumens/backend/service/JobService.java
@@ -3,6 +3,14 @@
  */
 package com.lumens.backend.service;
 
+import com.lumens.backend.ServerUtils;
+import com.lumens.io.JsonUtility;
+import com.lumens.sysdb.DAOFactory;
+import com.lumens.sysdb.dao.JobDAO;
+import com.lumens.sysdb.entity.Job;
+import com.lumens.sysdb.entity.Project;
+import com.lumens.sysdb.utils.DBHelper;
+import java.util.List;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -10,6 +18,7 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
+import org.codehaus.jackson.JsonGenerator;
 
 /**
  *
@@ -21,7 +30,39 @@ public class JobService {
     @GET
     @Produces("application/json")
     public Response listJob() {
-        return Response.ok().entity("{ 'do' : 'list' }").build();
+        try {
+            JsonUtility utility = JsonUtility.createJsonUtility();
+            JsonGenerator json = utility.getGenerator();
+            json.writeStartObject();
+            json.writeStringField("status", "OK");
+            json.writeObjectFieldStart("content");
+            json.writeArrayFieldStart("jobs");
+            JobDAO jobDAO = DAOFactory.getJobDAO();
+            List<Job> jobList = jobDAO.getAllJob();
+            for (Job job : jobList) {
+                json.writeStartObject();
+                json.writeStringField("id", Long.toString(job.id));
+                json.writeStringField("name", job.name);
+                json.writeStringField("description", job.description);
+                json.writeArrayFieldStart("projects");
+                List<Project> projectList = DBHelper.loadShortProjectFromDb(job.id);
+                for (Project project : projectList) {
+                    json.writeStartObject();
+                    json.writeNumberField("project_id", project.id);
+                    json.writeStringField("project_name", project.name);
+                    json.writeStringField("project_description", project.description);
+                    json.writeEndObject();
+                }
+                json.writeEndArray();
+                json.writeEndObject();
+            }
+            json.writeEndArray();
+            json.writeEndObject();
+            json.writeEndObject();
+            return Response.ok().entity(utility.toUTF8String()).build();
+        } catch (Exception e) {
+            return ServerUtils.getErrorMessageResponse(e);
+        }
     }
 
     @GET
