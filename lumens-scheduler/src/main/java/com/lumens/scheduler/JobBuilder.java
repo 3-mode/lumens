a77a8d5ed012ce5d41c1d4bf47627f9b2fd82b15
xiaoxin <whiskeyfly@163.com>
2015-03-09 08:39:34 +0800

    Add jobBuilder

---
+++ lumens-scheduler/src/main/java/com/lumens/scheduler/JobBuilder.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright Lumens Team, Inc. All Rights Reserved.
+ */
+
+package com.lumens.scheduler;
+
+/**
+ *
+ * @author Xiaoxin(whiskeyfly@163.com)
+ */
+public interface JobBuilder {
+    Job build();
+    JobBuilder withJobId(long jobId);
+    JobBuilder withJobName(String jobName);
+    JobBuilder withJobDescripttion(String jobDescription);
+}
