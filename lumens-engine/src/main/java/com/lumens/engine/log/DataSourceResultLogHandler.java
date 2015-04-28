56767ccef817e1f7d7d050513a2c462f075df59c
shaofeng-wang <shaofeng.wang@outlook.com>
2015-04-28 08:34:35 +0800

    Log element data in log file

--- lumens-engine/src/main/java/com/lumens/engine/log/DataSourceResultLogHandler.java
+++ lumens-engine/src/main/java/com/lumens/engine/log/DataSourceResultLogHandler.java
@@ -4,6 +4,7 @@
 package com.lumens.engine.log;
 
 import com.lumens.engine.TransformComponent;
+import com.lumens.engine.TransformEngineContext;
 import com.lumens.engine.component.resource.DataSource;
 import com.lumens.engine.handler.DataSourceResultHandler;
 import com.lumens.logsys.LogSysFactory;
@@ -20,10 +21,21 @@ public class DataSourceResultLogHandler implements DataSourceResultHandler {
 
     @Override
     public void processOutput(TransformComponent src, String targetName, List<Element> output) {
+        if (log.isDebugEnabled())
+            log.debug(String.format("Datasource '%s' output size '%d' target => '%s'", src.getName(), output.size(), targetName));
+
+        if (TransformEngineContext.getContext().isLogElement()) {
+
+        }
     }
 
     @Override
     public void processInput(TransformComponent src, String targetName, List<Element> input) {
-    }
+        if (log.isDebugEnabled())
+            log.debug(String.format("Datasource '%s' input size '%d' target => '%s'", src.getName(), input.size(), targetName));
 
+        if (TransformEngineContext.getContext().isLogElement()) {
+
+        }
+    }
 }
