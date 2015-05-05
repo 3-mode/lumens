6d0b45f2a50e8d87982a2e71c9fe948040f1f99f
shaofeng-wang <shaofeng.wang@outlook.com>
2015-05-05 09:51:22 +0800

    Remove log error because the error already thrown to up level

--- lumens-engine/src/main/java/com/lumens/engine/log/TransformComponentInOutLogHandler.java
+++ lumens-engine/src/main/java/com/lumens/engine/log/TransformComponentInOutLogHandler.java
@@ -48,7 +48,6 @@ public class TransformComponentInOutLogHandler implements DataSourceResultHandle
                     log.info(utility.toUTF8String());
                 }
             } catch (Exception ex) {
-                log.error(ex);
                 throw new RuntimeException(ex);
             }
         }
