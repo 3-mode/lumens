572a406cac7f0c6dec6557c43235e1c148d76c86
shaofeng-wang <shaofeng.wang@outlook.com>
2015-05-20 10:45:25 +0800

    Add override for method

--- lumens-connector-logminer/src/main/java/com/lumens/connector/logminer/impl/DefaultLogMiner.java
+++ lumens-connector-logminer/src/main/java/com/lumens/connector/logminer/impl/DefaultLogMiner.java
@@ -38,6 +38,7 @@ public class DefaultLogMiner implements LogMiner, Constants {
         }
     }
 
+    @Override
     public void buildDictionary() {
         try {
             if (config.getDictType() == DICT_TYPE.STORE_IN_FILE) {
