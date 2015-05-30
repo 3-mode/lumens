85d5d588c3c4fd28044334cfe666fbd0cde02677
wiguo <wiguo@SHA-L7-WIGUO.amd.com>
2015-05-30 17:31:56 +0800

    Change user_tab_comments to all_tab_comments

--- lumens-connector-orcl/src/main/java/com/lumens/connector/database/client/oracle/OracleConstants.java
+++ lumens-connector-orcl/src/main/java/com/lumens/connector/database/client/oracle/OracleConstants.java
@@ -22,6 +22,7 @@ public interface OracleConstants extends DBConstants {
     public String NUMBERIC = "NUMBERIC";
     public String BLOB = "BLOB";
     // Oracle SQL to query tables information
-    public String TABLENAMES = "select t.table_name,t.comments, t.TABLE_TYPE from user_tab_comments t";
-    public String TABLECOLUMNS = "select t.COLUMN_NAME, t.DATA_TYPE, t.DATA_LENGTH from user_tab_columns t where t.TABLE_NAME = '%s'";
+    // ALL_TAB_COMMENTS displays comments on the tables and views accessible to the current user.
+    public String TABLENAMES = "select t.table_name,t.comments, t.TABLE_TYPE from all_tab_comments t";
+    public String TABLECOLUMNS = "select t.COLUMN_NAME, t.DATA_TYPE, t.DATA_LENGTH from all_tab_comments t where t.TABLE_NAME = '%s'";
 }
