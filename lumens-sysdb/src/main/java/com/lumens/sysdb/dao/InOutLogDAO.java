f322aa995bd5898c010c161bcbe8395b03a5a7fb
shaofeng-wang <shaofeng.wang@outlook.com>
2015-05-31 21:54:35 +0800

    Fix the value special char issue with prepared statement

--- lumens-sysdb/src/main/java/com/lumens/sysdb/dao/InOutLogDAO.java
+++ lumens-sysdb/src/main/java/com/lumens/sysdb/dao/InOutLogDAO.java
@@ -12,10 +12,10 @@ import java.util.List;
  */
 public class InOutLogDAO extends BaseDAO {
     public void create(final InOutLogItem inoutLogItem) {
-        this.simpleTransactionExecute(sqlManager.getSQL("InOutLogDAO/PutLog", inoutLogItem.logID,
-                                                        inoutLogItem.componentID, inoutLogItem.componentName,
-                                                        inoutLogItem.projectID, inoutLogItem.projectName, inoutLogItem.direction,
-                                                        inoutLogItem.targetName, inoutLogItem.data, inoutLogItem.lastModifTime));
+        this.simplePrepareStatTransactionExecute(sqlManager.getSQL("InOutLogDAO/PutLog"), inoutLogItem.logID,
+                                                 inoutLogItem.componentID, inoutLogItem.componentName,
+                                                 inoutLogItem.projectID, inoutLogItem.projectName, inoutLogItem.direction,
+                                                 inoutLogItem.targetName, inoutLogItem.data, inoutLogItem.lastModifTime);
     }
 
     public List<InOutLogItem> getLogList(long projectID, long componentID) {
