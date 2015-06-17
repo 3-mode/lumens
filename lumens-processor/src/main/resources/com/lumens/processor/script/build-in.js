d5a01a6f46fa2b0b131b0708fb12c0c58286abc2
shaofeng-wang <shaofeng.wang@outlook.com>
2015-06-17 10:23:59 +0800

    Merge with git 2

--- lumens-processor/src/main/resources/com/lumens/processor/script/build-in.js
+++ lumens-processor/src/main/resources/com/lumens/processor/script/build-in.js
@@ -9,25 +9,38 @@ function getElementValue(ctx, path) {
     return ScriptUtils.getElement(ctx, path);
 }
 
-function logInfo(message) {
+function $LogInfo(message) {
     // TODO need log4j here
     System.out.println(message);
 }
 
 /*Get the current date*/
-function now() {
+function $Now() {
     var cal = Calendar.getInstance();
     return cal.getTime();
 }
 
 /*Format a date to string*/
-function dateToString(date, format) {
+function $DateToString(date, format) {
     var dateFormat = new SimpleDateFormat(format);
     return dateFormat.format(date);
 }
 
 /*Format a string to data*/
-function stringToDate(string, format) {
+function $StringToDate(string, format) {
     var dateFormat = new SimpleDateFormat(format);
     return dateFormat.parse(string);
+}
+
+function $Accessory(ctx, name, data) {
+    if (data === undefined) {
+        // Get data
+    }
+    else {
+        // Put data
+    }
+}
+
+function $RemoveAccessory(ctx, name) {
+
 }
